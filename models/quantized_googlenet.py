63a9a2df1f6169a6bfa1c81cf5d8b2cf8f6d297d
Đinh Ngọc Ân <101492362+andythetechnerd03@users.noreply.github.com>
2023-07-29 11:01:26 +0700

    update py

---
+++ models/quantized_googlenet.py
@@ -0,0 +1,21 @@
+import torch
+from torch import nn
+from models.googlenet import GoogleNet
+
+class Quantized_Googlenet(nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+        self.quant = torch.ao.quantization.QuantStub()
+        self.dequant = torch.ao.quantization.DeQuantStub()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # Quantize the input tensor then feed into the model then de-quantize it.
+        x = self.quant(x)
+        x = self.model(x)
+        x = self.dequant(x)
+
+        return x
+    
+def quantized_googlenet():
+    return Quantized_Googlenet(GoogleNet())
